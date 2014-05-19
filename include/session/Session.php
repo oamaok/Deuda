f101ca824b6022b3785847e8446c97e7d943e823
Teemu Pääkkönen <paakkonen.teemu@gmail.com>
2014-05-19 15:56:37 +0300

    added Session:: and some comments

--- include/session/Session.php
+++ include/session/Session.php
@@ -3,7 +3,32 @@
 
 class Session {
 
+    /**
+     * @var User
+     * Stores user data if one is logged in.
+     *
+     */
+
     private static $sessionUser = null;
+
+    /**
+     * @var string
+     *
+     * Is set or unset when user logs in or logs out.
+     * This way we instantly know whether the session cookie
+     * is set ot not. Without it a page refresh is needed
+     * in order to get the cookie value.
+     *
+     */
+
+    private static $manualToken = null;
+
+    /**
+     * @return mixed
+     *
+     * Fetches the session user if one is logged in.
+     * Otherwise returns null.
+     */
 
     public static function getUser()
     {
@@ -69,18 +94,35 @@ class Session {
      */
     private static function setSessionCookie($token, $expireTime)
     {
+        Session::$manualToken = $token;
         return setcookie(Config::SESSION_COOKIE, $token, $expireTime);
     }
 
+    /**
+     * @return null|string
+     *
+     * Returns the session cookie if one is set.
+     *
+     */
+
     private static function getSessionCookie()
     {
+        if(Session::$manualToken)
+            return Session::$manualToken;
         if(!isset($_COOKIE[Config::SESSION_COOKIE]))
             return null;
         return $_COOKIE[Config::SESSION_COOKIE];
     }
 
+    /**
+     * @return bool
+     *
+     * Deletes the session cookie.
+     */
+
     private static function deleteSessionCookie()
     {
+        Session::$manualToken = null;
         return setcookie(Config::SESSION_COOKIE, null, -1);
     }
 }
