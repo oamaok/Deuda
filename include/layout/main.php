7dcc3b6c10ae3b2fb53f5dc44fdf8c7596415a7d
Teemu Pääkkönen <paakkonen.teemu@gmail.com>
2014-05-22 01:30:26 +0300

    added layout

---
+++ include/layout/main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <base href="<?= Config::SITE_BASE ?>">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+    <title><?= Deuda::$pageTitle ?></title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
+</head>
+<body>
+<?= Deuda::getNavigation() ?>
+<div class="container">
+    <?= Deuda::$outputBuffer ?>
+</div>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</body>
+</html>
